c4ba784ede392aff6705d362dd0f8a5894060a19
Rashedul Islam <rashedul.rajon@gmail.com>
2023-06-18 16:14:16 +0600

    bug Fixed

--- server/app.js
+++ server/app.js
@@ -1,28 +1,49 @@
-//basic lib import
-const { readdirSync } = require("fs");
-const path = require("path")
-const express = require("express");
-const app = express();
-const mongoSanitize =require('express-mongo-sanitize');
-const helmet = require("helmet");
+//Basic lib import
+const express = require('express');
+const app = new express();
+const bodyParser =require('body-parser');
 const morgan = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const rateLimit = require('express-rate-limit');
 
 //middleware
-app.use(cors());
-app.use(mongoSanitize());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet());
 
-//request rate limit
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
+//Security middleware lib import
+const rateLimit =require('express-rate-limit');
+const mongoSanitize =require('express-mongo-sanitize');
+const helmet =require('helmet');
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Database lib import
+const mongoose = require('mongoose');
+app.use(mongoSanitize());
+app.use(express.static('client/build'));
+const {readdirSync} = require("fs");
+const path = require("path");
+
+//Security middleware implement
+app.use(morgan("dev"));
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+app.use(hpp())
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
+//Body Parser Implement
+app.use(bodyParser.json());
+
+//Request Rate Limit
+const limiter = rateLimit({windowMs:15*60*1000, max:3000});
 app.use(limiter);
 
-//mongodb connection
+//mongoDB Database Connection
 let URI="mongodb+srv://<username>:<password>@cluster0.aw6azwi.mongodb.net/ostad-com?retryWrites=true&w=majority";
 let OPTION={user:'rashedul',pass:'170174Rajon',autoIndex:true}
 mongoose
@@ -35,18 +56,15 @@ mongoose
         console.log(err.message)
     });
 
-//routing implement
-readdirSync("./src/routes").map(r => app.use("/api/v1", require(`./src/routes/${r}`)));
+//Routing Implement
+readdirSync("./src/routes").map(r=>app.use("/api/v1", require(`./src/routes/${r}`)));
 
-//undefined route implement
-app.use("*", (req, res) => {
-    res.status(404).json({ status: "failed", data: "Not Found" })
-});
+
 
 // Add React Front End Routing
-
-app.use(express.static('client/build'));
 app.get('*',function (req,res) {
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
 })
+
+
 module.exports = app;
